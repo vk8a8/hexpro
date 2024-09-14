424cf9e0e54dfcd9221520938e31a8895e08c0f9
vk8a8 <vk8a8@protonmail.com>
2024-09-14 16:55:33 -0700

    now its REALLY simple (:<

--- main.c
+++ main.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <errno.h>
 
 int main(int argc, char* argv[]) {
 	const char hexchars[17] = "0123456789ABCDEF";
 	char lsd;
 	char msd;
-	char hexdec[3];
 	
 	char* outname;
-	if (argc == 3) outname = argv[2];
+	if (argc > 2) outname = argv[2];
 	else outname = "out.txt";
-
-	if (argc > 3)
-	{
-		fprintf(stderr, "Error: E2BIG %d\n", E2BIG);
-		return E2BIG;
-	}
 
 	FILE* outfptr = fopen(outname, "w");
 	FILE* infptr = fopen(argv[1], "rb");
@@ -25,12 +17,10 @@ int main(int argc, char* argv[]) {
 	
 	while ((ch = fgetc(infptr)) != EOF)
 	{
-		lsd = ch & 0xF;
-		msd = ch >>  4;
-		hexdec[0] = hexchars[msd];
-		hexdec[1] = hexchars[lsd];
+		lsd = hexchars[ch & 0xF];
+		msd = hexchars[ch >>  4];
 
-		fprintf(outfptr,hexdec);
+		fprintf(outfptr,"%c%c ", msd, lsd);
 	}
 	fclose(outfptr);
 
