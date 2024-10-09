650229b7f610987b094d6c1c91028be617839dc4
vk8a8 <vk8a8@protonmail.com>
2024-10-09 13:02:47 -0700

    Completed migration to c

--- main.c
+++ main.c
@@ -1,28 +1,25 @@
-#include <fstream>
-#include <cstring>
-#include <iostream>
+#include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 void printHelp() {
-	printf("Usage: hexpro <input file> [options]\n");
-	printf("Options:\n");
-	printf("	-h, --help		Show this message and exit\n");
-	printf("	-o FILE			Specify output file\n");
-	printf("	-l LENGTH		Specify line length in byte pairs\n");
+	puts("Usage: hexpro <input file> [options]");
+	puts("Options:");
+	puts("	-h, --help		Show this message and exit");
+	puts("	-o FILE			Specify output file");
+	puts("	-l LENGTH		Specify line length in byte pairs");
 }
 
 int main(int argc, char* argv[]) {
-	constexpr char hc[17] = "0123456789ABCDEF";
-
-	using std::string, std::istreambuf_iterator;
+	const char hc[17] = "0123456789ABCDEF";
 
 	char lsd; // least sig digit
 	char msd; // most sig digit
 	char* inname;
-	char linelength = 16;
+	int linelength = 16;
 	char* outname = "out.txt";
 
-	for ( int i = 1; i < argc; i++ ) // whatever
+	for ( int i = 1; i < argc; i++ ) // args
 	{
 		if ( !strcmp( argv[i], "-h") || !strcmp( argv[i], "--help") ) {
 			printHelp();
@@ -32,36 +29,31 @@ int main(int argc, char* argv[]) {
 			outname = argv[i];
 		} else if ( !strcmp( argv[i], "-l") ) {
 			i++;
-			linelength = static_cast<char>( strtol(argv[i], NULL, 10) );
+			linelength = (int) strtol(argv[i], NULL, 10);
 		}
 		else
 			inname = argv[i];
-		
 	}
 
 	short chunkll = linelength * 3;
 
-	std::ifstream ifs(inname);
-	string content( (istreambuf_iterator<char>(ifs) ),
-			(istreambuf_iterator<char>()));
-
 	FILE* outfptr = fopen(outname, "w");
-	FILE* infptr = fopen(argv[1], "rb");
+	FILE* infptr = fopen(inname, "rb");
 
 	fseek(infptr, 0, SEEK_END);
 	long fsize = ftell(infptr);
 	fseek(infptr, 0, SEEK_SET);
 
-	char bytec = 0;
-	char bytes[1 + chunkll] = { 0 };
+	unsigned int bytec = 0;
+	char bytes[chunkll + 1];
 
-	memset(bytes, ' ', linelength * 3);
+	memset(bytes, ' ', sizeof(bytes));
+	bytes[chunkll] = '\0';
+	bytes[chunkll - 1] = '\n';
 
-//	for ( long i = 0; i < fsize; i++ )
 	int p;
 	while ((p = fgetc(infptr)) != EOF)
 	{
-//		unsigned char ch = content[i];
 		unsigned char ch = (unsigned char) p;
 		lsd = hc[ch & 0xF];	// abcdABCD & 00001111 -> 0000ABCD
 		msd = hc[ch >> 4];	// abcdABCD >> 4 -> 0000abcd (uchar makes it not preserve first bit)
@@ -69,12 +61,22 @@ int main(int argc, char* argv[]) {
 		bytes[3 * bytec] = msd;
 		bytes[3 * bytec + 1] = lsd;
 
-		if (bytec == linelength - 1 ) {
-			fprintf(outfptr,"%s\n", bytes);
+		if (bytec == linelength - 1) {
+			fputs(bytes, outfptr);
 			bytec = 0;
+			memset(bytes, ' ', sizeof(bytes));
+			bytes[chunkll] = '\0';
+			bytes[chunkll - 1] = '\n';
 		} else bytec++;
 
 	}
+
+	if (bytec > 0) {
+		bytes[3 * bytec] = '\n';
+		bytes[3 * bytec + 1] = '\0';
+		fputs(bytes, outfptr);
+	}
+
 	fclose(infptr);
 	fclose(outfptr);
 
